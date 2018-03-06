ebeb3dfdbbaf92f5d49b8b7fe87ffd70622a06c6
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-03-06 16:51:38 +0900

    #14 P2P Server on top of Express

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require("express"),
   bodyParser = require("body-parser"),
   morgan = require("morgan"),
-  Blockchain = require("./blockchain");
+  Blockchain = require("./blockchain"),
+  P2P = require("./p2p");
 
 const { getBlockchain, createNewBlock } = Blockchain;
+const { startP2PServer } = P2P;
 
 // Psssst. Don't forget about typing 'export HTTP_PORT=4000' in your console
 const PORT = process.env.HTTP_PORT || 3000;
@@ -22,4 +24,8 @@ app.post("/blocks", (req, res) => {
   res.send(newBlock);
 });
 
-app.listen(PORT, () => console.log(`Nomadcoin Server running on ${PORT} ✅`));
+const server = app.listen(PORT, () =>
+  console.log(`Nomadcoin HTTP Server running on port ${PORT} ✅`)
+);
+
+startP2PServer(server);
