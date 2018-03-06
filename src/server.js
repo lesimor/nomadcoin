6072369b5b852037ba0df4c0a92c343c87f93216
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-03-06 15:24:09 +0900

    #12 Mining our first block (Blockchain API)

--- src/server.js
+++ src/server.js
@@ -10,4 +10,15 @@ const PORT = 3000;
 const app = express();
 app.use(bodyParser.json());
 app.use(morgan("combined"));
-app.listen(PORT, () => console.log(´Nomadcoin Server running on ${PORT}´));
+
+app.get("/blocks", (req, res) => {
+  res.send(getBlockchain());
+});
+
+app.post("/blocks", (req, res) => {
+  const { body: { data } } = req;
+  const newBlock = createNewBlock(data);
+  res.send(newBlock);
+});
+
+app.listen(PORT, () => console.log(`Nomadcoin Server running on ${PORT} ✅`));
