4fa81504708b05be02666e4a9e1f3246c4e2c659
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-03-06 13:59:57 +0900

    Creating Genesis Block

--- src/blockchain.js
+++ src/blockchain.js
@@ -7,3 +7,15 @@ class Block {
     this.data = data;
   }
 }
+
+const genesisBlock = new Block(
+  0,
+  "2C4CEB90344F20CC4C77D626247AED3ED530C1AEE3E6E85AD494498B17414CAC",
+  null,
+  1520312194926,
+  "This is the genesis!!"
+);
+
+let blockchain = [genesisBlock];
+
+console.log(blockchain);
