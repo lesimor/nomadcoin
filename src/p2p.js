85cd3d27b33d73a2d8dee332ecdfdab86070e13b
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-03-06 19:07:39 +0900

    #22 Syncing Chains part One

--- src/p2p.js
+++ src/p2p.js
@@ -1,7 +1,12 @@
 const WebSockets = require("ws"),
   Blockchain = require("./blockchain");
 
-const { getNewestBlock, isBlockStructureValid, replaceChain } = Blockchain;
+const {
+  getNewestBlock,
+  isBlockStructureValid,
+  replaceChain,
+  getBlockchain
+} = Blockchain;
 
 const sockets = [];
 
@@ -69,6 +74,9 @@ const handleSocketMessages = ws => {
       case GET_LATEST:
         sendMessage(ws, responseLatest());
         break;
+      case GET_ALL:
+        sendMessage(ws, responseAll());
+        break;
       case BLOCKCHAIN_RESPONSE:
         const receivedBlocks = message.data;
         if (receivedBlocks === null) {
@@ -95,7 +103,7 @@ const handleBlockchainResponse = receivedBlocks => {
     if (newestBlock.hash === latestBlockReceived.previousHash) {
       addBlockToChain(latestBlockReceived);
     } else if (receivedBlocks.length === 1) {
-      // to do, get all the blocks, we are waaaay behind
+      sendMessageToAll(getAll());
     } else {
       replaceChain(receivedBlocks);
     }
@@ -104,7 +112,12 @@ const handleBlockchainResponse = receivedBlocks => {
 
 const sendMessage = (ws, message) => ws.send(JSON.stringify(message));
 
-const responseLatest = () => blockchainResponse([getLastBlock()]);
+const sendMessageToAll = message =>
+  sockets.forEach(ws => sendMessage(ws, message));
+
+const responseLatest = () => blockchainResponse([getNewestBlock()]);
+
+const responseAll = () => blockchainResponse(getBlockchain());
 
 const handleSocketError = ws => {
   const closeSocketConnection = ws => {
