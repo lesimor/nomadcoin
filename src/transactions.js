6a88eddf58ecc9373eac16467119590b3a956e5b
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-03-08 19:43:14 +0900

    #59 Bug fixing for the Coinbase Transaction

--- src/transactions.js
+++ src/transactions.js
@@ -261,13 +261,13 @@ const validateCoinbaseTx = (tx, blockIndex) => {
   }
 };
 
-const createCoinbaseTx = (adress, blockIndex) => {
+const createCoinbaseTx = (address, blockIndex) => {
   const tx = new Transaction();
   const txIn = new TxIn();
   txIn.signature = "";
   txIn.txOutId = blockIndex;
-  tx.txIns[txIn];
-  tx.txOut = [new TxOut(address, COINBASE_AMOUNT)];
+  tx.txIns = [txIn];
+  tx.txOuts = [new TxOut(address, COINBASE_AMOUNT)];
   tx.id = getTxId(tx);
   return tx;
 };
